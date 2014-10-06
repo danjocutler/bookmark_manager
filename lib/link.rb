c251b9a00592f7887716458609bd86497c504533
danjocutler <danjocutler@gmail.com>
2014-10-06 17:12:47 +0100

    Added link and data_cleaner files

---
+++ lib/link.rb
@@ -0,0 +1,11 @@
+#This class corresponds to a table in the database
+class Link
+
+	include DataMapper::Resource
+
+	#this block will have the resources our models will have
+	property :id, Serial #Serial means it will auto-increment for each record
+	property :title, String
+	property :url, String
+
+end
