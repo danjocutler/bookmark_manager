eef22312c65014c4eb022fd809ff58e4c55f2a37
danjocutler <danjocutler@gmail.com>
2014-10-06 19:00:35 +0100

    Added filter by tag

--- lib/link.rb
+++ lib/link.rb
@@ -3,10 +3,11 @@ class Link
 
 	include DataMapper::Resource
 
+	has n, :tags, :through => Resource
+	
 	#this block will have the resources our models will have
 	property :id, Serial #Serial means it will auto-increment for each record
 	property :title, String
 	property :url, String
-	has n, :tags, :through => Resource
 
 end
