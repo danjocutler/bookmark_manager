da8c8015e4fd58bafb476707caa1115fe3701514
danjocutler <danjocutler@gmail.com>
2014-10-08 16:04:03 +0100

    Moved files and Refactored

--- app/server.rb
+++ app/server.rb
@@ -1,13 +1,18 @@
 require 'sinatra'
 require 'data_mapper'
 require 'rack-flash'
-require 'sinatra-partial'
-require './lib/link'
-require './lib/tag'
-require './lib/user'
+require 'sinatra/partial'
+require './app/models/link'
+require './app/models/tag'
+require './app/models/user'
+require_relative 'data_mapper_setup'
 require_relative 'helpers/application'
 require_relative 'helpers/session'
-require_relative 'data_mapper_setup'
+require_relative 'controllers/application'
+require_relative 'controllers/links'
+require_relative 'controllers/sessions'
+require_relative 'controllers/tags'
+require_relative 'controllers/users'
 
 include SessionHelpers
 
@@ -15,69 +20,17 @@ set :views, Proc.new { File.join(root, "..", "app/views") }
 enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash
+set :partial_template_engine, :erb
 
-get '/' do
-	@links = Link.all
-	erb :index
-end
 
-post '/links' do
-  url = params["url"]
-  title = params["title"]
-  tags = params["tags"].split(" ").map {|tag| Tag.first_or_create(:text => tag)}
-  Link.create(:url => url, :title => title, :tags => tags)
-  redirect to('/')
-end
 
-get '/tags/:text' do
-	tag = Tag.first(:text => params[:text])
-	@links = tag ? tag.links : []
-	erb :index
-end
 
-get '/users/new' do
-  @user = User.new
-  erb :"users/new"
-  # note the view is in views/users/new.erb
-  # we need the quotes because otherwise
-  # ruby would divide the symbol :users by the
-  # variable new (which makes no sense)
-end
 
-post '/users' do
-	@user = User.new(:email => params[:email],
-				      :password => params[:password],
-              :password_confirmation => params[:password_confirmation])
-  if @user.save
-  	session[:user_id] = @user.id
-  	redirect to('/')
-  else
-    flash.now[:errors] = @user.errors.full_messages
-    erb :"users/new"
-  end
-end	
 
-get '/sessions/new' do
-  erb :"sessions/new"
-end
 
-post '/sessions' do
-  email, password = params[:email], params[:password]
-  user = User.authenticate(email, password)
-  if user
-    session[:user_id] = user.id
-    redirect to '/'
-  else
-    flash[:errors] = ["The email or password is incorrect"]
-    erb :"sessions/new"
-  end
-end 
 
-  delete '/sessions' do
-    flash[:notice] = "Goodbye!"
-    session[:user_id] = nil
-    redirect '/'
-  end 
+
+
 
 
 
